bf2ba2d6787bd0e893931a77b5fd07ba88187578
Ridcjtk <ridcjtk@gmail.com>
2021-02-27 11:26:55 -0800

    Second commit

--- fanout.py
+++ fanout.py
@@ -1,8 +1,16 @@
-def difference_fanout(list, fanout):
-    newlist = []
-    first = 0
-    while first < len(list):
-     for val in list[first+1 : ]:
-       newlist.append(val-fanout)
-       first+1
-    return newlist
+def difference_fanout(beans, fanout):
+    endlist = []
+    for x, value in enumerate(beans):
+      newlist = []
+      for val in beans[x+1: x+4]:
+        newlist.append(val- value)
+
+      endlist.append(newlist)
+
+    return endlist
+
+
+
+    
+result = difference_fanout([3, 2, 4, 6, 1], 3)
+print(result)
